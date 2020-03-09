f419fa7784e8781029a09fcafabc534fffe93aec
Mohamed <MohamedEalbd>
2020-03-10 00:44:38 +0200

    speedometer

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData.dark(),
-      title: 'Flutter Demo',
       home: SpeedMeter()
     );
   }
